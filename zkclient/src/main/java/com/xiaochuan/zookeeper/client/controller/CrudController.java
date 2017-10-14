e86cb5e33406cbe858a9f66494585903170c0d6b
wangxiaochuan <wangxiaochuan@corp.netease.com>
2017-10-14 15:56:53 +0800

    update readme

--- zkclient/src/main/java/com/xiaochuan/zookeeper/client/controller/CrudController.java
+++ zkclient/src/main/java/com/xiaochuan/zookeeper/client/controller/CrudController.java
@@ -1,10 +1,15 @@
 package com.xiaochuan.zookeeper.client.controller;
 
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.zookeeper.data.Stat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 @SuppressWarnings("unused")
 @RestController
@@ -12,9 +17,33 @@ public class CrudController {
     @Autowired
     CuratorFramework client;
 
+    @RequestMapping("/node/getdata")
+    Map<String, Object> getZNodeData(@RequestParam("path") String path) throws Exception {
+        Map<String, Object> m = new HashMap<>();
+        m.put("result", new String(client.getData().forPath(path)));
+        m.put("status", client.checkExists().forPath(path));
+
+        return m;
+    }
+
+    @RequestMapping("/node/setdata")
+    Map<String, Object> getZNodeData(@RequestParam("path") String path,
+                                   @RequestParam("data") String data) throws Exception {
+        Map<String, Object> m = new HashMap<>();
+
+        m.put("result", client.setData().forPath(path, data.getBytes()));
+        m.put("status", client.checkExists().forPath(path));
+
+        return m;
+    }
+
     @RequestMapping("/node/add")
-    String create(@RequestParam("name") String name,
-                  @RequestParam("data") String data) throws Exception {
-        return client.create().forPath(name, data.getBytes());
+    Map<String, Object> createZNode(@RequestParam("path") String path,
+                                  @RequestParam("data") String data) throws Exception {
+        Map<String, Object> m = new HashMap<>();
+        m.put("result", client.create().forPath(path, data.getBytes()));
+        m.put("status", client.checkExists().forPath(path));
+
+        return m;
     }
 }
