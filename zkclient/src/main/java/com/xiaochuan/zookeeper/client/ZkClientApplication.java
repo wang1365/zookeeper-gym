f5cad1b000b59ad3db0badb719b2301a21627f69
wangxiaochuan <wangxiaochuan@corp.netease.com>
2017-10-14 17:42:53 +0800

    code refactor

--- zkclient/src/main/java/com/xiaochuan/zookeeper/client/ZkClientApplication.java
+++ zkclient/src/main/java/com/xiaochuan/zookeeper/client/ZkClientApplication.java
@@ -1,34 +1,12 @@
 package com.xiaochuan.zookeeper.client;
 
-import org.apache.curator.RetryPolicy;
-import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.retry.RetryNTimes;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ZkClientApplication {
-    Logger logger = LoggerFactory.getLogger(ZkClientApplication.class);
-
-    // TODO: when this bean is destroyed, bean's "close" is not invoked
-    @Bean
-    CuratorFramework createZkClient() {
-        logger.info("##### Start create zk client");
-        RetryPolicy policy = new RetryNTimes(3, 1000);
-        CuratorFramework client = CuratorFrameworkFactory.builder()
-                .connectString("127.0.0.1")
-                .retryPolicy(policy)
-                .build();
-        client.start();
-        return client;
-    }
 
     public static void main(String[] args) throws Exception {
-
         SpringApplication.run(ZkClientApplication.class, args);
     }
 
---
+++ zkclient/src/main/java/com/xiaochuan/zookeeper/client/zkclient/ZkClientConfiguration.java
@@ -0,0 +1,35 @@
+package com.xiaochuan.zookeeper.client.zkclient;
+
+import com.xiaochuan.zookeeper.client.ZkClientApplication;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@SuppressWarnings("unused")
+@Configuration
+public class ZkClientConfiguration {
+    private static final Logger logger = LoggerFactory.getLogger(ZkClientApplication.class);
+
+    @Value("${zookeeper.server}")
+    private String server;
+
+    // TODO: when this bean is destroyed, bean's "close" is not invoked
+    @Bean
+    CuratorFramework createZkClient() {
+        logger.info("##### Start create zk client");
+        RetryPolicy policy = new RetryNTimes(3, 1000);
+        CuratorFramework client = CuratorFrameworkFactory.builder()
+                .connectString(server)
+                .retryPolicy(policy)
+                .build();
+        client.start();
+        return client;
+    }
+}
