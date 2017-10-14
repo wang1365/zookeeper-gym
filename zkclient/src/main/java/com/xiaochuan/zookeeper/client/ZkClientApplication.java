8f2ddb21a23acc70e6604ef15c62cfaae94557d9
wangxiaochuan <wangxiaochuan@corp.netease.com>
2017-10-14 11:57:25 +0800

    add missing files

--- zkclient/src/main/java/com/xiaochuan/zookeeper/client/ZkClientApplication.java
+++ zkclient/src/main/java/com/xiaochuan/zookeeper/client/ZkClientApplication.java
@@ -1,12 +1,28 @@
 package com.xiaochuan.zookeeper.client;
 
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZkClientApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Logger logger = LoggerFactory.getLogger(ZkClientApplication.class);
+
+        RetryPolicy policy = new RetryNTimes(3, 1000);
+        CuratorFramework client = CuratorFrameworkFactory.newClient("127.0.0.1:2181", policy);
+        client.start();
+        byte[] data = client.getData().forPath("/client1");
+        logger.info("Zookeeper node data: {}", new String(data));
+        client.close();
+
         SpringApplication.run(ZkClientApplication.class, args);
+        logger.info("######## zk client close.");
     }
 
 }
