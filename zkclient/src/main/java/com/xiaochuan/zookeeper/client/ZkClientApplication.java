92734e9601e3855e76a88dd05f3be60d8fbe1a51
wangxiaochuan <wangxiaochuan@corp.netease.com>
2017-10-14 11:16:02 +0800

    init spring boot application

---
+++ zkclient/src/main/java/com/xiaochuan/zookeeper/client/ZkClientApplication.java
@@ -0,0 +1,12 @@
+package com.xiaochuan.zookeeper.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZkClientApplication.class, args);
+    }
+
+}
